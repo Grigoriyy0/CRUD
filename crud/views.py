9cffbcdbc1b58c3a8f95532405283403efc83c46
Grigoriyy0 <padgamerrfp@gmail.com>
2024-05-15 23:01:35 +0400

    Renewed index page, form

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import UserForm
-# Create your views here.
+
 def index(request):
-    userForm = UserForm()
-    return render(request, 'crud/index.html', {'form':userForm})
+    if request.method == 'POST':
+        name = request.get('name')
+        age = request.get('age')
+        return HttpResponse(f'<h2> Привет, {name}, твой возраст: {age} </h2>')
+    else:
+        userform = UserForm()
+        return render(request, 'crud/index.html', {'form':userform})
