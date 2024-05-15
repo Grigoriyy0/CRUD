54fea8c133bc3fefd0a6612a3e452eee30951f19
Grigoriyy0 <padgamerrfp@gmail.com>
2024-05-15 22:50:40 +0400

    Created form app, html page

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .forms import UserForm
 # Create your views here.
+def index(request):
+    userForm = UserForm()
+    return render(request, 'crud/index.html', {'form':userForm})
